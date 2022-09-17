443edd73528dcd1f6436a0537abb94f39ad95944
raghavrawat98 <49871652+raghavrawat98@users.noreply.github.com>
2022-09-18 02:32:37 +0530

    Able to get Params

--- DeskmanCS/CommandPage.aspx.cs
+++ DeskmanCS/CommandPage.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -15,8 +16,95 @@ namespace DeskmanCS
         }
         protected void CommandTextBox_TextChanged(object sender, EventArgs e)
         {
-            string t = CommandTextBox.Text;
-            TestLabel.Text = t;
+            string string_CommandTextBox = CommandTextBox.Text;
+            if (!CustomeBalancedParenthesis(string_CommandTextBox))
+            {
+                TestLabel.Text=@"Sorry expression not balaned :/";
+                return;
+
+            }
+            string[] DelimitedStrings = string_CommandTextBox.Split('{', '}');
+
+
+            int bracketCounter = 0;
+
+            List<string> paramStrings = new List<string>();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in DelimitedStrings)
+            {
+                bracketCounter++;
+                if (bracketCounter % 2 == 0)
+                {
+                    sb.AppendLine(s);
+                    paramStrings.Add(s);
+                }
+            }
+            TestLabel.Text = sb.ToString().Replace(Environment.NewLine, "<br />");
         }
+        static Boolean CustomeBalancedParenthesis(string exp)
+        {
+            // Declare an empty character stack */
+            Stack<char> st = new Stack<char>();
+
+            // Traverse the given expression to
+            //   check matching brackets
+            for (int i = 0; i < exp.Length; i++)
+            {
+                // If the exp[i] is a starting
+                // bracket then push it
+                if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
+                    st.Push(exp[i]);
+
+                //  If exp[i] is an ending bracket
+                //  then pop from stack and check if the
+                //   popped bracket is a matching pair
+                if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']')
+                {
+
+                    // If we see an ending bracket without
+                    //   a pair then return false
+                    if (st.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    // Pop the top element from stack, if
+                    // it is not a pair brackets of
+                    // character then there is a mismatch. This
+                    // happens for expressions like {(})
+                    else if (!isMatchingPair(st.Pop(), exp[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // If there is something left in expression
+            // then there is a starting bracket without
+            // a closing bracket
+
+            if (st.Count == 0)
+                return true; // balanced
+            else
+            {
+                // not balanced
+                return false;
+            }
+        }
+        static Boolean isMatchingPair(char character1,
+                                 char character2)
+        {
+            if (character1 == '(' && character2 == ')')
+                return true;
+            else if (character1 == '{' && character2 == '}')
+                return true;
+            else if (character1 == '[' && character2 == ']')
+                return true;
+            else
+                return false;
+        }
+
+
     }
 }
